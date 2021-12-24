9a5857e149cfa3f487ef43ea587de1325bea17e7
andy <a5706617@gmail.com>
2021-12-24 19:56:44 +0800

    Initial Commit

---
+++ 1A2B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1A2B
+//
+//  Created by 江啟綸 on 2021/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
